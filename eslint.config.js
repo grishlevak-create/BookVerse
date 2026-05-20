c392b62471c59c50fe5de0624b0a6c73ce55162e
Grigory <grishlevak@gmail.com>
2026-05-20 22:30:16 +0300

    fix: eslint ignore scripts

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,16 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage', 'vite.config.ts', 'jest.config.cjs', 'jest.setup.cjs'] },
+  {
+    ignores: [
+      'dist',
+      'coverage',
+      'vite.config.ts',
+      'jest.config.cjs',
+      'jest.setup.cjs',
+      'scripts/**',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
